73946a08643f8fa37777fd659b61e39379661f9c
Deml <thomad@10ddb1ba91e6.ant.amazon.com>
2016-10-07 10:02:10 -0700

    make install script work on all Mac installs

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
-echo "copying source to /usr/local/aws/lib/python2.7/site-packages/awscli/customizations directory"
-sudo rm -rf /usr/local/aws/lib/python2.7/site-packages/awscli/customizations/kinesis
-sudo mkdir /usr/local/aws/lib/python2.7/site-packages/awscli/customizations/kinesis
-sudo cp -r kinesis/*  /usr/local/aws/lib/python2.7/site-packages/awscli/customizations/kinesis/
+USR_LOCATION=/usr/local/aws/lib/python2.7/site-packages/awscli/customizations
+LIB_LOCATION=/Library/Python/2.7/site-packages/awscli/customizations/
+
+if [ -e $USR_LOCATION ] 
+  then
+    echo "copying source to $USR_LOCATION directory" 
+    sudo cp -R kinesis  $USR_LOCATION 
+fi
+
+if [ -e $LIB_LOCATION ]
+  then
+    echo "Copying sources to $LIB_LOCATION directory"
+    sudo cp -R kinesis $LIB_LOCATION
+fi
