4e3c07fe828e4e0263e9cff08ad3682af96ebb39
Deml <thomad@10ddb1ba91e6.ant.amazon.com>
2016-10-07 16:30:13 -0700

    checking in non-working version because I need to drink beer

--- kinesis/getshardmetrics.py
+++ kinesis/getshardmetrics.py
@@ -25,6 +25,16 @@ class GetShardMetricsCommand(BasicCommand):
         {'name': 'stream-name', 'required': True, 'help_text': 'The name of the stream'}
     ]
 
+    def aws_generic_client(self, service_name, globals):
+      client = self.aws_client(
+        service_name, 
+        globals.region, 
+        globals.endpoint_url, 
+        globals.verify_ssl
+      )
+      return client
+      
+ 
     def aws_client(self, service_name, region, endpoint_url, verify_ssl):
       client = self._session.create_client(
         service_name, 
@@ -34,32 +44,62 @@ class GetShardMetricsCommand(BasicCommand):
       )
       return client
  
-    def get_stream_shards(self, kinesis_client, stream_name):
-      return 0
+    def get_shard_ids_for_stream(self, kinesis_client, stream_name):
+      response = kinesis_client.describe_stream(
+        StreamName = stream_name
+      )
+      #BUG BUG - do we need to handle > 100 shards, i.e. pagination!
+      shard_ids = []
+      for shard in response['StreamDescription']['Shards']:
+        shard_ids.append(shard['ShardId'])
+      return shard_ids
+        
 
     def _run_main(self, args, parsed_globals):
-      cloudwatch_client = self.aws_client('cloudwatch', parsed_globals.region, parsed_globals.endpoint_url, parsed_globals.verify_ssl)
-      kinesis_client = self.aws_client('kinesis', parsed_globals.region, parsed_globals.endpoint_url, parsed_globals.verify_ssl)
-       
-      stream_shards = self.get_stream_shards(kinesis_client, args.stream_name)
 
-      response = cloudwatch_client.get_metric_statistics(
-          Namespace = 'AWS/Kinesis',
-          StartTime = datetime.datetime.utcnow() - datetime.timedelta(minutes=15),
-          EndTime = datetime.datetime.utcnow(),
-          MetricName = 'IncomingRecords',
-          Period = 60,
-          Statistics = ['Sum'],
-          Dimensions=[
-            {
-              'Name': 'StreamName',
-              'Value': args.stream_name
-            },
-          ],
-      )
-      sys.stdout.write(
-        str(response['Datapoints'])
-      )
+      kinesis_client = self.aws_generic_client('kinesis', parsed_globals)
+      cloudwatch_client = self.aws_generic_client('cloudwatch', parsed_globals)
+ 
+      shard_ids = self.get_shard_ids_for_stream(kinesis_client, args.stream_name)
+
+      datapoints = {} 
+      start_time = datetime.datetime.utcnow() - datetime.timedelta(minutes=15)
+      end_time = datetime.datetime.utcnow()
+
+      for shard_id in shard_ids:
+        response = cloudwatch_client.get_metric_statistics(
+            Namespace = 'AWS/Kinesis',
+            StartTime = start_time,
+            EndTime = end_time,
+            MetricName = 'IncomingRecords',
+            Period = 60,
+            Statistics = ['Sum'],
+            Dimensions=[
+              {
+                'Name': 'StreamName',
+                'Value': args.stream_name
+              },
+              {
+                'Name': 'ShardId',
+                'Value': shard_id
+              },
+            ],
+        )
+        entries = response['Datapoints']
+        if len(entries) > 0:
+          sums = map(lambda x: int(x['Sum']), entries)
+          entries['avg'] = sum(sums) / len(sums)
+          #entries['max'] = max(sums)
+          print 'here'
+          #print 'Shard id {0}: {1}, avg: {2}, max: {3}'.format(
+          #  shard_id, 
+          #  sums,
+          #  entries['avg'],
+          #  entries['max']
+          #)
+        else:
+          print 'No data for shard id {0}'.format(shard_id)
+        #datapoints[shard_id] = entries
       return 0
 
 
