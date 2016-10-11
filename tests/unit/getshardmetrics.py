018c5f3e465b6e416c0d75c4f9367438cb08bca0
Deml <thomad@10ddb1ba91e6.ant.amazon.com>
2016-10-11 16:36:35 -0700

    adding unit tests

--- tests/unit/getshardmetrics.py
+++ tests/unit/getshardmetrics.py
@@ -1,7 +1,23 @@
+import tempfile
+import datetime
+from mock import Mock 
+
 from awscli.testutils import unittest
 from kinesis.getshardmetrics import GetShardMetricsCommand
 
 
-class GetShardMetrics(unittest.TestCase):
+class TestGetShardMetrics(unittest.TestCase):
  def setUp(self):
-        self.rootdir = tempfile.mkdtemp()
+   self.rootdir = tempfile.mkdtemp()
+   self.mock_args = Mock(
+     stream_name = 'test',
+     metric_name = 'IncomingRecords',
+     statistic = 'Sum',
+     start_time = datetime.datetime.utcnow() - datetime.timedelta(minutes=15),
+     end_time = datetime.datetime.utcnow(),
+  )
+
+ def test_collect_args(self):
+   collect_args(self.mock_args)
+      
+ 
