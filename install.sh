aab6d2ccbfcdd1bd2cdb5f59c35ec3e1bfe54172
Deml <thomad@3c07541ecc48.ant.amazon.com>
2016-10-17 21:21:44 -0700

    changed directory location of unit tests to not have issues with the unit tests

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-USR_LOCATION=/usr/local/aws/lib/python2.7/site-packages/awscli/customizations
+USR_LOCATION=/usr/local/aws/lib/python2.7/site-packages/awscli/customizations/
 LIB_LOCATION=/Library/Python/2.7/site-packages/awscli/customizations/
 
 if [ -e $USR_LOCATION ] 
   then
     echo "copying source to $USR_LOCATION directory" 
-    sudo cp -R kinesis  $USR_LOCATION 
+    sudo cp -R awscli/customizations/kinesis  $USR_LOCATION 
 fi
 
 if [ -e $LIB_LOCATION ]
   then
     echo "Copying sources to $LIB_LOCATION directory"
-    sudo cp -R kinesis $LIB_LOCATION
+    sudo cp -R awscli/customizations/kinesis $LIB_LOCATION
 fi
