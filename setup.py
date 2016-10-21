6fed3bf907c4e043546e5da187577fcbe36355bc
Deml <thomad@3c07541ecc48.ant.amazon.com>
2016-10-20 22:01:46 -0700

    trying with packages

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(
     name='kinesis_cli_plugin',
     version='0.1',
-    package_dir = {'': 'kinesis_cli_plugin'},
     packages = ['kinesis'],
     install_requires=[
         'awscli',
