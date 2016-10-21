54088d386e13b2c28118c35330886d81305c19d4
Deml <thomad@3c07541ecc48.ant.amazon.com>
2016-10-20 22:00:12 -0700

    trying with packages

--- setup.py
+++ setup.py
@@ -3,15 +3,8 @@ from setuptools import setup
 setup(
     name='kinesis_cli_plugin',
     version='0.1',
-    py_modules=[
-      'kinesis',
-      'kinesis/__init__',
-      'kinesis/getshardmetrics',
-      'kinesis/shardmetrics', 
-      'kinesis/shardmetricsgetter',
-      'kinesis/timestringconverter',
-      'kinesis/__init__.py'
-    ],
+    package_dir = {'': 'kinesis_cli_plugin'}
+    packages = ['kinesis'],
     install_requires=[
         'awscli',
     ],
