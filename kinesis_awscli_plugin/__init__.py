343f2b6de8ce5499dc71419d0b0eeb56f8c997d8
Deml <thomad@3c07541ecc48.ant.amazon.com>
2016-10-22 21:52:59 -0700

    bug fix in push command

--- kinesis_awscli_plugin/__init__.py
+++ kinesis_awscli_plugin/__init__.py
@@ -1,6 +1,7 @@
 from awscli.customizations.commands import BasicCommand
 from kinesis_awscli_plugin.getshardmetrics import GetShardMetricsCommand
 from kinesis_awscli_plugin.pull import PullCommand
+from kinesis_awscli_plugin.push import PushCommand
 
 def awscli_initialize(event_emitter):
     event_emitter.register('building-command-table.kinesis', inject_commands)
@@ -8,3 +9,4 @@ def awscli_initialize(event_emitter):
 def inject_commands(command_table, session, **kwargs):
     command_table['get-shard-metrics'] = GetShardMetricsCommand(session)
     command_table['pull'] = PullCommand(session)
+    command_table['push'] = PushCommand(session)
