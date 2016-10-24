74adf3c4a9ed3241b84b3a41a493cd5fe3d1ba5e
Deml <thomad@3c07541ecc48.ant.amazon.com>
2016-10-23 22:59:51 -0700

    adding some more data to setup

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='kinesis_awscli_plugin',
-    version='0.1',
+    name = 'kinesis_awscli_plugin',
+    version = '0.1',
     packages = ['kinesis_awscli_plugin'],
-    install_requires=[
+    install_requires = [
         'awscli',
     ],
+    author = 'Thomas Deml',
+    author_email = 'thomas.deml@gmail.com',
+    description = 'An AWS Command-line Interface plugin for AWS Kinesis',
+    keywords = 'AWS Kinesis CLI',
 )
