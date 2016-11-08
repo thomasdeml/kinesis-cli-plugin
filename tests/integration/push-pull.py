03db9aabe67ce33e9328235164b5c404cf999a59
Deml <thomad@3c07541ecc48.ant.amazon.com>
2016-11-08 09:05:37 -0800

    fixed integration test

--- tests/integration/push-pull.py
+++ tests/integration/push-pull.py
@@ -18,11 +18,13 @@ class TestPushPull:
     self.delete_stream()
 
   def test_push(self):
-    command = 'for i in {1..50};do echo "Hello World $i";sleep 0.1;done | aws kinesis push --disable-batch --push-delay 100 --stream-name ' + self.stream_name
+    command = 'for i in {1..10};do echo "$i ";sleep 0.1;done | aws kinesis push --disable-batch --push-delay 100 --stream-name ' + self.stream_name
     command_output = os.popen(command).read()
     assert '.....' in command_output
+    print("here")
     stream_records = self.get_all_stream_records()
-    assert '.....' in stream_records
+    print("STREAM RECORDS: %s" % stream_records)
+    assert '1 2 3 4 5 6 7 8 9 10' in stream_records
 
   def create_stream(self):
     if self.stream_name in self.kinesis.list_streams()['StreamNames']:
@@ -49,4 +51,31 @@ class TestPushPull:
     self.kinesis.delete_stream(StreamName=self.stream_name)
 
   def get_all_stream_records(self):
-    return '.....'
+    all_records = []
+
+    self.next_shard_iterator = self.get_shard_iterator_from_trim_horizon()
+    params = dict(ShardIterator=self.next_shard_iterator)
+    while True:
+      gr_response = self.kinesis.get_records(**params)
+      if gr_response:
+        records = gr_response['Records']
+        print('Behind latest: %s' % gr_response['MillisBehindLatest'])
+        if len(records) == 0:
+          print('No records read')
+        else:
+          print('Adding records to the queue')
+          all_records.extend(records)                   
+        if gr_response['MillisBehindLatest'] < 1000:
+          print all_records
+          return ''.join(map(lambda record: record['Data'], all_records))
+        self.next_shard_iterator = gr_response['NextShardIterator']
+      else:
+        print('empty response')
+  
+  def get_shard_iterator_from_trim_horizon(self):
+    params = dict(
+      StreamName = self.stream_name, 
+      ShardIteratorType = 'TRIM_HORIZON',
+      ShardId = 'shardId-000000000000',
+    )
+    return self.kinesis.get_shard_iterator(**params)['ShardIterator']
